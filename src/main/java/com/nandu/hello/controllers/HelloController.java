e9f7ededce96e5df36d2fb8db9b74b2433e41d6d
jfbueno <buenojeferson@outlook.com>
2022-10-20 11:37:37 -0300

    Primeiro commit

---
+++ src/main/java/com/nandu/hello/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.nandu.hello.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Index!";
+    }
+}
